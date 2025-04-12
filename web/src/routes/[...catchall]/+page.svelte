3ccce8611b250207046ec6c6cc8e4b5f6c340958
Martin Fejt <martin@fejt.cz>
2025-04-12 16:55:30 +0200

    add error page

--- web/src/routes/[...catchall]/+page.svelte
+++ web/src/routes/[...catchall]/+page.svelte
@@ -7,15 +7,15 @@
 </script>
 
 <div
-    class="min-h-screen flex flex-col items-center bg-[#2e2e2e] text-gray-100 px-6"
+    class="min-h-screen flex flex-col items-center bg-[#2e2e2e] text-gray-100 mt-20 px-6"
 >
     <h1 class="text-6xl font-bold text-[#f6ad55] mb-4">{status}</h1>
     <p class="text-xl mb-6">{error?.message || "Došlo k chybě!"}</p>
 
     {#if status === 404}
-        <p class="text-gray-400 mb-4">
-            Tuto funkci jsme nenašli. Zkontrolujte prosím URL adresu nebo se
-            vraťte na dashboard.
+        <p class="text-gray-400 text-center mb-4">
+            Tuto funkci jsme nenašli. <br />
+            Zkontrolujte prosím URL adresu nebo se vraťte na dashboard.
         </p>
         <a
             href="/"
