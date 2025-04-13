e6f6bbabe511a1598d321196189fdd3ec205d64c
Martin Fejt <martin@fejt.cz>
2025-04-13 03:56:56 +0200

    add https

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		https: {
+			key: fs.readFileSync('./cert.key'),
+			cert: fs.readFileSync('./cert.crt'),
+		},
+	}
 });
